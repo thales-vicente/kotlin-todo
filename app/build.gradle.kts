c950e02ca56bdba2afe1da31de32e04c91c53e05
Thales-Vicente <thalesvicentebem10@hotmail.com>
2025-03-19 17:14:46 -0300

    Add Navigation Fragment KTX and Navigation UI KTX dependencies and create nav_graph.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,8 @@ dependencies {
     implementation(libs.androidx.credentials)
     implementation(libs.androidx.credentials.play.services.auth)
     implementation(libs.googleid)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
