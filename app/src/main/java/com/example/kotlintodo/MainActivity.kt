0632521bfe79970d64de81ed110b09752d25abde
Thales-Vicente <thalesvicentebem10@hotmail.com>
2025-03-19 17:06:03 -0300

    add fragments and layouts

--- app/src/main/java/com/example/kotlintodo/MainActivity.kt
+++ app/src/main/java/com/example/kotlintodo/MainActivity.kt
@@ -16,32 +16,5 @@ import com.example.kotlintodo.ui.theme.KotlinTodoTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            KotlinTodoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KotlinTodoTheme {
-        Greeting("Android")
     }
 }
