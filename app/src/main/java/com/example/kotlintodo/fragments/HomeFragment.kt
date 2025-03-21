f52f6dd4c9f633af7f65e21ffebe47b736c77511
Thales-Vicente <thalesvicentebem10@hotmail.com>
2025-03-21 09:07:47 -0300

    Implement RecyclerView to display tasks in HomeFragment

--- app/src/main/java/com/example/kotlintodo/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/kotlintodo/fragments/HomeFragment.kt
@@ -12,18 +12,26 @@ import com.example.kotlintodo.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kotlintodo.databinding.FragmentHomeBinding
 import com.example.kotlintodo.fragments.AddTodoPopupFragment.DialogNextBtnClickListener
+import com.example.kotlintodo.utils.ToDoAdapter
+import com.example.kotlintodo.utils.ToDoData
 import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
-class HomeFragment : Fragment(), DialogNextBtnClickListener {
+class HomeFragment : Fragment(), DialogNextBtnClickListener, ToDoAdapter.ToDoAdapterClicksInterface {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var databaseRef: DatabaseReference
     private lateinit var navController: NavController
     private lateinit var binding: FragmentHomeBinding
     private lateinit var popupFragment: AddTodoPopupFragment
+    private lateinit var adapter: ToDoAdapter
+    private lateinit var mList: MutableList<ToDoData>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,6 +45,7 @@ class HomeFragment : Fragment(), DialogNextBtnClickListener {
         super.onViewCreated(view, savedInstanceState)
 
         init(view)
+        getDataFromFirebase()
         registerEvents()
     }
 
@@ -56,6 +65,33 @@ class HomeFragment : Fragment(), DialogNextBtnClickListener {
         auth = FirebaseAuth.getInstance()
         databaseRef = FirebaseDatabase.getInstance().reference.child("Tasks")
             .child(auth.currentUser?.uid.toString())
+        binding.recyclerView.setHasFixedSize(true)
+        binding.recyclerView.layoutManager = LinearLayoutManager(context)
+        mList = mutableListOf()
+        adapter = ToDoAdapter(mList)
+        adapter.setListener(this)
+        binding.recyclerView.adapter = adapter
+    }
+    private fun getDataFromFirebase(){
+        databaseRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                mList.clear()
+                for (taskSnapshot in snapshot.children){
+                    val todoTask = taskSnapshot.key?.let {
+                        ToDoData(it, taskSnapshot.value.toString())
+                    }
+                    if (todoTask != null){
+                        mList.add(todoTask)
+                    }
+                }
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(context, error.message, Toast.LENGTH_SHORT).show()
+            }
+
+        })
     }
 
     override fun onSaveTask(todo: String, todoEt: TextInputEditText) {
@@ -67,5 +103,19 @@ class HomeFragment : Fragment(), DialogNextBtnClickListener {
             }
             popupFragment.dismiss()
         }
+    }
+
+    override fun onDeleteTaskBtnClicked(toDoData: ToDoData) {
+        databaseRef.child(toDoData.taskId).removeValue().addOnCompleteListener {
+            if (it.isSuccessful){
+                Toast.makeText(context, "Deleted Successfully", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(context, it.exception?.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onEditTaskBtnClicked(toDoData: ToDoData) {
+        TODO("Not yet implemented")
     }
 }
---
+++ app/src/main/java/com/example/kotlintodo/utils/ToDoData.kt
@@ -0,0 +1,3 @@
+package com.example.kotlintodo.utils
+
+data class ToDoData(val taskId: String, val task: String)
